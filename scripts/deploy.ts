6afc4f4c0547ccd1b58474cc4a6d92e519283cd5
Yash Agrawal <yashagrawal18@outlook.com>
2021-10-18 12:37:46 +0530

    Remove unncessary files

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -13,21 +13,7 @@ async function main() {
   // manually to make sure everything is compiled
   // await hre.run('compile');
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
-
-  const HelloWorld = await ethers.getContractFactory("HelloWorld");
-  const contract = await HelloWorld.deploy();
-
-  const FixedSwap = await ethers.getContractFactory('FixedSwap')
-  const fixedSwap = await FixedSwap.deploy()
-
-  await greeter.deployed();
-  await contract.deployed()
-  fixedSwap.deployed()
-
-  console.log("Greeter deployed to:", greeter.address);
+  const FixedSwap = await ethers.getContractFactory("FixedSwap");
 }
 
 // We recommend this pattern to be able to use async/await everywhere
