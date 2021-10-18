fc50801731a40926a36c94a21d3a2f8c23b561ef
Yash Agrawal <yashagrawal18@outlook.com>
2021-10-18 14:03:04 +0530

    Minor fixes

--- contracts/test/MockERC20.sol
+++ contracts/test/MockERC20.sol
@@ -5,7 +5,9 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
 
 contract MockERC20 is ERC20PresetMinterPauser {
-    constructor() ERC20PresetMinterPauser("ScallopX", "SCLP") {
+    constructor(string memory name, string memory symbol)
+        ERC20PresetMinterPauser(name, symbol)
+    {
         _mint(msg.sender, 10000 * 10**18);
     }
 }
