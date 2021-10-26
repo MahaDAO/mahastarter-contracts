f2308948569f86bc3e699e8af07faadb4db4e4d1
Yash Agrawal <yashagrawal18@outlook.com>
2021-10-27 00:52:53 +0530

    More fix

--- scripts/whitelistAddresses.ts
+++ scripts/whitelistAddresses.ts
@@ -4,7 +4,7 @@ async function main() {
   console.log(`\nBeginnning Whitelisting script on network ${network.name}...\n`);
 
   const deployment = require(`../output/${network.name}.json`);
-  const contractKeys: string[] = Object.values(deployment);
+  const contractKeys: string[] = Object.keys(deployment);
   const whilelistAddresses = process.env.WHITELIST_ADDRESSES?.split(`,`) || [];
 
   for (const contractKey of contractKeys) {
