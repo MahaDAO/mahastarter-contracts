2640856156bb5393a8c4ec73c9f805f09d07977a
Yash Agrawal <yashagrawal18@outlook.com>
2021-12-14 13:28:07 +0530

    feat: minor updates in the scripts

--- scripts/whitelistAddresses.ts
+++ scripts/whitelistAddresses.ts
@@ -13,52 +13,50 @@ async function main() {
   const mahaxt2 = await fs.readFile(path.resolve(__dirname, "../output/mahaxt2.txt"));
   const squareRaw = await fs.readFile(path.resolve(__dirname, "../output/squareRaw.txt"));
 
-
   const mahaxAddresses = mahaxRaw.toString().split("\n");
   const publicAddresses = publicRaw.toString().split("\n");
   const mahaxt2Addresses = mahaxt2.toString().split("\n");
   const squareRawAddresses = squareRaw.toString().split("\n");
 
+  console.log(`\nAdding to whitelist for FORWARDforwardmahaxFixedSwap Fixedswap...`);
+  for (let i = 0; i < mahaxAddresses.length; i += 500) {
+    const addressse = mahaxAddresses.slice(i, i + 500);
+    const addressFinal = addressse.filter((a) => isAddress(a));
+    console.log(i, addressFinal.length - 1, addressFinal[0], addressFinal[addressFinal.length - 1]);
 
-  // console.log(`\nAdding to whitelist for SCLPscallopmahaxFixedSwap Fixedswap...`);
-  // for (let i = 0; i < mahaxAddresses.length; i += 500) {
-  //   const addressse = mahaxAddresses.slice(i, i + 500);
-  //   const addressFinal = addressse.filter((a) => isAddress(a));
-  //   console.log(i, addressFinal.length - 1, addressFinal[0], addressFinal[addressFinal.length - 1]);
+    const fixedSwap = await ethers.getContractAt("FixedSwap", deployment.FORWARDforwardmahaxFixedSwap.address);
+    await fixedSwap.add(addressFinal);
+  }
 
-  //   const fixedSwap = await ethers.getContractAt("FixedSwap", deployment.FORWARDforwardmahaxFixedSwap.address);
-  //   await fixedSwap.add(addressFinal);
-  // }
+  console.log(`\nAdding to whitelist for FORWARDforwardFixedSwap Fixedswap...`);
+  for (let i = 0; i < publicAddresses.length; i += 500) {
+    const addressse = publicAddresses.slice(i, i + 500);
+    const addressFinal = addressse.filter((a) => isAddress(a));
+    console.log(i, addressFinal.length - 1, addressFinal[0], addressFinal[addressFinal.length - 1]);
 
-  // console.log(`\nAdding to whitelist for SCLPscallopFixedSwap Fixedswap...`);
-  // for (let i = 0; i < publicAddresses.length; i += 500) {
-  //   const addressse = publicAddresses.slice(i, i + 500);
-  //   const addressFinal = addressse.filter((a) => isAddress(a));
-  //   console.log(i, addressFinal.length - 1, addressFinal[0], addressFinal[addressFinal.length - 1]);
+    try {
+      const fixedSwap = await ethers.getContractAt("FixedSwap", deployment.FORWARDforwardFixedSwap.address);
+      await fixedSwap.add(addressFinal);
+    } catch (error) {
+      console.log("whitelist failed for i = ", i);
+    }
+  }
 
-  //   try {
-  //     const fixedSwap = await ethers.getContractAt("FixedSwap", deployment.FORWARDforwardFixedSwap.address);
-  //     await fixedSwap.add(addressFinal);
-  //   } catch (error) {
-  //     console.log("whitelist failed for i = ", i);
-  //   }
-  // }
+  console.log(`\nAdding to whitelist for FORWARDforwardmahax2FixedSwap Fixedswap...`);
+  for (let i = 0; i < mahaxt2Addresses.length; i += 500) {
+    const addressse = mahaxt2Addresses.slice(i, i + 500);
+    const addressFinal = addressse.filter((a) => isAddress(a));
+    console.log(i, addressFinal.length - 1, addressFinal[0], addressFinal[addressFinal.length - 1]);
 
-  // console.log(`\nAdding to whitelist for SCLPscallopFixedSwap Fixedswap...`);
-  // for (let i = 0; i < mahaxt2Addresses.length; i += 500) {
-  //   const addressse = mahaxt2Addresses.slice(i, i + 500);
-  //   const addressFinal = addressse.filter((a) => isAddress(a));
-  //   console.log(i, addressFinal.length - 1, addressFinal[0], addressFinal[addressFinal.length - 1]);
+    try {
+      const fixedSwap = await ethers.getContractAt("FixedSwap", deployment.FORWARDforwardmahax2FixedSwap.address);
+      await fixedSwap.add(addressFinal);
+    } catch (error) {
+      console.log("whitelist failed for i = ", i);
+    }
+  }
 
-  //   try {
-  //     const fixedSwap = await ethers.getContractAt("FixedSwap", deployment.FORWARDforwardmahax2FixedSwap.address);
-  //     await fixedSwap.add(addressFinal);
-  //   } catch (error) {
-  //     console.log("whitelist failed for i = ", i);
-  //   }
-  // }
-
-  console.log(`\nAdding to whitelist for SCLPscallopFixedSwap Fixedswap...`);
+  console.log(`\nAdding to whitelist for FORWARDforwardsquareupFixedSwap Fixedswap...`);
   for (let i = 0; i < squareRawAddresses.length; i += 500) {
     const addressse = squareRawAddresses.slice(i, i + 500);
     const addressFinal = addressse.filter((a) => isAddress(a));
--- scripts/withdrawFunds.ts
+++ scripts/withdrawFunds.ts
@@ -1,17 +1,15 @@
 import { ethers, network } from "hardhat";
-import fs from "fs/promises";
-import path from "path";
-import { isAddress } from "./utils";
 
 async function main() {
-  console.log(`\Withdrawing raised funds on network ${network.name}...\n`);
+  console.log(`\nWithdrawing raised funds on network ${network.name}...\n`);
 
   const deployment = require(`../output/${network.name}.json`);
 
-  const fixedSwap1 = await ethers.getContractAt("FixedSwap", deployment.SCLPscallopFixedSwap.address);
-  const fixedSwap2 = await ethers.getContractAt("FixedSwap", deployment.SCLPscallopmahaxFixedSwap.address);
-  console.log(await fixedSwap1.pause());
-  console.log(await fixedSwap2.pause());
+  const fixedSwap1 = await ethers.getContractAt("FixedSwap", deployment.FORWARDforwardFixedSwap.address);
+  const fixedSwap2 = await ethers.getContractAt("FixedSwap", deployment.FORWARDforwardmahaxFixedSwap.address);
+
+  await fixedSwap1.withdrawFunds();
+  await fixedSwap2.withdrawFunds();
 }
 
 main().catch((error) => {
